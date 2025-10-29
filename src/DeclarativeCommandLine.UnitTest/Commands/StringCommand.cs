6860c63334b5a1ca089132bc6cf7990cd8fd19df
Marco van den Oever <marco@flyingpie.nl>
2025-10-29 01:51:48 +0100

    Default values

--- src/DeclarativeCommandLine.UnitTest/Commands/StringCommand.cs
+++ src/DeclarativeCommandLine.UnitTest/Commands/StringCommand.cs
@@ -1,6 +1,27 @@
+using DeclarativeCommandLine.UnitTest.Utils;
+
 namespace DeclarativeCommandLine.UnitTest.Commands;
 
 [Command(Parent = typeof(AppRootCommand))]
 public class StringCommand
 {
+	[Command(Parent = typeof(MathCommand))]
+	public class AddCommand(IOutput output) : ICommand
+	{
+		[Option(
+			AllowedValues = ["value-1", "value-2", "value-3"],
+			Description = "The first value.",
+			Required = true)]
+		public int ValueA { get; set; }
+
+		[Option(
+			Description = "The second value.",
+			Required = true)]
+		public int ValueB { get; set; }
+
+		public void Execute()
+		{
+			output.WriteLine($"{ValueA} + {ValueB} = {ValueA + ValueB}");
+		}
+	}
 }
---
+++ src/DeclarativeCommandLine.UnitTest/ScratchTest.cs
@@ -0,0 +1,33 @@
+using System.CommandLine;
+
+namespace DeclarativeCommandLine.UnitTest;
+
+[TestClass]
+public class ScratchTest
+{
+	[TestMethod]
+	public void Test1()
+	{
+		var rootCmd = new RootCommand();
+		rootCmd.Hidden = false;
+		// global::DeclarativeCommandLine.UnitTest.Commands.AliasesCommand
+		{
+			var cmd1 = new Command("aliases");
+			rootCmd.Add(cmd1);
+			cmd1.Hidden = false;
+
+			var opt1 = new Option<String>("--grand-child-argument-a");
+			{
+				cmd1.Add(opt1);
+				opt1.Description = "";
+				opt1.Hidden = false;
+				opt1.Required = false;
+				opt1.DefaultValueFactory = argRes => "";
+
+				opt1.AcceptOnlyFromAmong([]);
+				opt1.AcceptLegalFileNamesOnly();
+				opt1.AcceptLegalFilePathsOnly();
+			}
+		}
+	}
+}
