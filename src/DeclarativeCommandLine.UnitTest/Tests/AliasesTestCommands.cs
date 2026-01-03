e632ea89856f8038ab46ed2185f59ad802a14d43
Marco van den Oever <marco@flyingpie.nl>
2026-01-03 15:08:07 +0100

    Moved alias tests around again

--- src/DeclarativeCommandLine.UnitTest/Tests/AliasesTestCommands.cs
+++ src/DeclarativeCommandLine.UnitTest/Tests/AliasesTestCommands.cs
@@ -7,60 +7,55 @@ namespace DeclarativeCommandLine.UnitTest.Tests;
 [UsesVerify]
 public partial class AliasesTestCommands
 {
-	[TestClass]
-	[UsesVerify]
-	public partial class CommandTest
+	[TestMethod]
+	public async Task Aliases_Parent()
 	{
-		[TestMethod]
-		public async Task Aliases_Parent()
-		{
-			// Act
-			var res = await RunAsync(["test", "aliases", "commands"]);
+		// Act
+		var res = await RunAsync(["test", "aliases", "commands"]);
 
-			// Assert
-			await Verify(res.Output);
-			Assert.AreEqual(1, res.Code);
-		}
+		// Assert
+		await Verify(res.Output);
+		Assert.AreEqual(1, res.Code);
+	}
 
-		[TestMethod]
-		[DataRow("aliases-0")]
-		public async Task Aliases_0(string cmd)
-		{
-			// Act
-			var res = await RunAsync(["test", "aliases", "commands", cmd]);
+	[TestMethod]
+	[DataRow("aliases-0")]
+	public async Task Aliases_0(string cmd)
+	{
+		// Act
+		var res = await RunAsync(["test", "aliases", "commands", cmd]);
 
-			// Assert
-			await Verify(res.Output);
-			Assert.AreEqual(0, res.Code);
-		}
+		// Assert
+		await Verify(res.Output);
+		Assert.AreEqual(0, res.Code);
+	}
 
-		[TestMethod]
-		[DataRow("aliases-1")]
-		[DataRow("alias1")]
-		[SuppressMessage("Major Code Smell", "S4144:Methods should not have identical implementations", Justification = "MvdO: The output is different due to the command used.")]
-		public async Task Aliases_1(string cmd)
-		{
-			// Act
-			var res = await RunAsync(["test", "aliases", "commands", cmd]);
+	[TestMethod]
+	[DataRow("aliases-1")]
+	[DataRow("alias1")]
+	[SuppressMessage("Major Code Smell", "S4144:Methods should not have identical implementations", Justification = "MvdO: The output is different due to the command used.")]
+	public async Task Aliases_1(string cmd)
+	{
+		// Act
+		var res = await RunAsync(["test", "aliases", "commands", cmd]);
 
-			// Assert
-			await Verify(res.Output);
-			Assert.AreEqual(0, res.Code);
-		}
+		// Assert
+		await Verify(res.Output);
+		Assert.AreEqual(0, res.Code);
+	}
 
-		[TestMethod]
-		[DataRow("aliases-2")]
-		[DataRow("alias2-1")]
-		[DataRow("alias2-2")]
-		[SuppressMessage("Major Code Smell", "S4144:Methods should not have identical implementations", Justification = "MvdO: The output is different due to the command used.")]
-		public async Task Aliases_2(string cmd)
-		{
-			// Act
-			var res = await RunAsync(["test", "aliases", "commands", cmd]);
+	[TestMethod]
+	[DataRow("aliases-2")]
+	[DataRow("alias2-1")]
+	[DataRow("alias2-2")]
+	[SuppressMessage("Major Code Smell", "S4144:Methods should not have identical implementations", Justification = "MvdO: The output is different due to the command used.")]
+	public async Task Aliases_2(string cmd)
+	{
+		// Act
+		var res = await RunAsync(["test", "aliases", "commands", cmd]);
 
-			// Assert
-			await Verify(res.Output);
-			Assert.AreEqual(0, res.Code);
-		}
+		// Assert
+		await Verify(res.Output);
+		Assert.AreEqual(0, res.Code);
 	}
 }
