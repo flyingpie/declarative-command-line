7ffff2820fe1118f8206758622e5cb99c40f795d
Marco van den Oever <marco@flyingpie.nl>
2026-01-05 00:57:29 +0100

    Upgrade Nuke to version 10, and migrated sln to slnx

--- src/01-Build/NukeBuild/Build.cs
+++ src/01-Build/NukeBuild/Build.cs
@@ -2,7 +2,6 @@
 
 using JetBrains.Annotations;
 using Nuke.Common;
-using Nuke.Common.CI.GitHubActions;
 using Nuke.Common.Git;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
@@ -21,13 +20,16 @@ public sealed class Build : NukeBuild
 	[Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
 	private readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
-	[GitRepository] [Required]
+	[GitRepository]
+	[Required]
 	private readonly GitRepository GitRepository;
 
 	[Parameter("GitHub Token")]
 	private readonly string GitHubToken;
 
-	[Parameter("NuGet API Key")] [Secret] [CanBeNull]
+	[Parameter("NuGet API Key")]
+	[Secret]
+	[CanBeNull]
 	private readonly string NuGetApiKey;
 
 	[Parameter("NuGet Source")]
@@ -47,79 +49,79 @@ public sealed class Build : NukeBuild
 
 	private AbsolutePath PackageDirectory => OutputDirectory / "package";
 
-	[Solution(GenerateProjects = true, SuppressBuildProjectCheck = true)]
+	[Solution(GenerateProjects = true)]
 	private readonly Solution Solution;
 
-	private Target ReportInfo => _ => _
-		.Executes(() =>
-		{
-			Log.Information("Configuration:{Configuration}", Configuration);
-			Log.Information("PublicRelease:{PublicRelease}", PublicReleaseStr);
-		});
+	private Target ReportInfo =>
+		_ =>
+			_.Executes(() =>
+			{
+				Log.Information("Configuration:{Configuration}", Configuration);
+				Log.Information("PublicRelease:{PublicRelease}", PublicReleaseStr);
+			});
 
 	/// <summary>
 	/// Clean output directories.
 	/// </summary>
-	private Target Clean => _ => _
-		.DependsOn(ReportInfo)
-		.Executes(() =>
-		{
-			OutputDirectory.CreateOrCleanDirectory();
-		});
+	private Target Clean =>
+		_ =>
+			_.DependsOn(ReportInfo)
+				.Executes(() =>
+				{
+					OutputDirectory.CreateOrCleanDirectory();
+				});
 
 	/// <summary>
 	///
 	/// </summary>
-	private Target Test => _ => _
-		.DependsOn(Clean)
-		.Executes(() =>
-		{
-			DotNetTest(_ => _
-				.AddProcessAdditionalArguments("--solution", Solution.Path)
-				.SetConfiguration(Configuration)
-				.SetOutput("detailed")
-			);
-		});
+	private Target Test =>
+		_ =>
+			_.DependsOn(Clean)
+				.Executes(() =>
+				{
+					DotNetTest(_ =>
+						_.AddProcessAdditionalArguments("--solution", Solution.Path).SetConfiguration(Configuration).SetOutput("detailed")
+					);
+				});
 
 	/// <summary>
 	/// Build NuGet package.
 	/// </summary>
-	private Target Pack => _ => _
-		.DependsOn(Test)
-		.Produces(PackageDirectory)
-		.Executes(() =>
-		{
-			DotNetPack(_ => _
-				.SetConfiguration(Configuration)
-				.SetProject(Solution.DeclarativeCommandLine)
-				.SetOutputDirectory(PackageDirectory).SetProperty("PublicRelease", PublicReleaseStr)
-			);
-		})
-		.Executes(() =>
-		{
-			DotNetPack(_ => _
-				.SetConfiguration(Configuration)
-				.SetProject(Solution.DeclarativeCommandLine_Generator)
-				.SetOutputDirectory(PackageDirectory).SetProperty("PublicRelease", PublicReleaseStr)
-			);
-		});
+	private Target Pack =>
+		_ =>
+			_.DependsOn(Test)
+				.Produces(PackageDirectory)
+				.Executes(() =>
+				{
+					DotNetPack(_ =>
+						_.SetConfiguration(Configuration)
+							.SetProject(Solution.DeclarativeCommandLine)
+							.SetOutputDirectory(PackageDirectory)
+							.SetProperty("PublicRelease", PublicReleaseStr)
+					);
+				})
+				.Executes(() =>
+				{
+					DotNetPack(_ =>
+						_.SetConfiguration(Configuration)
+							.SetProject(Solution.DeclarativeCommandLine_Generator)
+							.SetOutputDirectory(PackageDirectory)
+							.SetProperty("PublicRelease", PublicReleaseStr)
+					);
+				});
 
 	/// <summary>
 	/// Push NuGet package.
 	/// </summary>
-	private Target Push => _ => _
-		.DependsOn(Pack)
-		.Produces(PackageDirectory)
-		.Executes(() =>
-		{
-			foreach (var nupkg in PackageDirectory.GlobFiles("*.nupkg"))
-			{
-				DotNetNuGetPush(p => p
-					.SetApiKey(NuGetApiKey)
-					.SetSkipDuplicate(true)
-					.SetSource(NuGetSource)
-					.SetTargetPath(nupkg)
-				);
-			}
-		});
+	private Target Push =>
+		_ =>
+			_.DependsOn(Pack)
+				.Produces(PackageDirectory)
+				.Executes(() =>
+				{
+					foreach (var nupkg in PackageDirectory.GlobFiles("*.nupkg"))
+					{
+						DotNetNuGetPush(p => p.SetApiKey(NuGetApiKey).SetSkipDuplicate(true).SetSource(NuGetSource).SetTargetPath(nupkg));
+					}
+				});
 }
