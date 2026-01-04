65b550f9a7663ce15e2066301bd27447a3f368e2
Marco van den Oever <marco@flyingpie.nl>
2026-01-05 00:16:33 +0100

    Default values test cleanup

--- src/DeclarativeCommandLine.UnitTest/Tests/DefaultValues/Options/OptionsTest.cs
+++ src/DeclarativeCommandLine.UnitTest/Tests/DefaultValues/Options/OptionsTest.cs
@@ -7,46 +7,41 @@ namespace DeclarativeCommandLine.UnitTest.Tests.DefaultValues.Options;
 [UsesVerify]
 public partial class OptionsTest
 {
-	[TestClass]
-	[UsesVerify]
-	public partial class IntsTest
+	[TestMethod]
+	public async Task Help()
 	{
-		[TestMethod]
-		public async Task Help()
-		{
-			// Act
-			var res = await RunAsync(["test", "default-values", "options", "--help"]);
+		// Act
+		var res = await RunAsync(["test", "default-values", "options", "--help"]);
 
-			// Assert
-			await Verify(res.Output);
-			Assert.AreEqual(0, res.Code);
-		}
+		// Assert
+		await Verify(res.Output);
+		Assert.AreEqual(0, res.Code);
+	}
 
-		[TestMethod]
-		public async Task Default()
-		{
-			// Act
-			var res = await RunAsync(["test", "default-values", "options"]);
+	[TestMethod]
+	public async Task Default()
+	{
+		// Act
+		var res = await RunAsync(["test", "default-values", "options"]);
 
-			// Assert
-			await Verify(res.Output);
-			Assert.AreEqual(0, res.Code);
-		}
+		// Assert
+		await Verify(res.Output);
+		Assert.AreEqual(0, res.Code);
+	}
 
-		// csharpier-ignore-start
-		[TestMethod]
-		[DataRow("--int",             "123")]
-		[DataRow("--int-nullable",    "123")]
-		[DataRow("--string",          "The Explicit String")]
-		// csharpier-ignore-end
-		public async Task Explicit(string opt, string val)
-		{
-			// Act
-			var res = await RunAsync(["test", "default-values", "options", opt, val]);
+	// csharpier-ignore-start
+	[TestMethod]
+	[DataRow("--int",             "123")]
+	[DataRow("--int-nullable",    "123")]
+	[DataRow("--string",          "The Explicit String")]
+	// csharpier-ignore-end
+	public async Task Explicit(string opt, string val)
+	{
+		// Act
+		var res = await RunAsync(["test", "default-values", "options", opt, val]);
 
-			// Assert
-			await Verify(res.Output);
-			Assert.AreEqual(0, res.Code);
-		}
+		// Assert
+		await Verify(res.Output);
+		Assert.AreEqual(0, res.Code);
 	}
 }
